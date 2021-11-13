507a92be440ef02dff909f1d9d0915a345ea0ad2
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2021-11-13 17:45:39 +0100

    Add `hello` subcommand and stub of `validate` subcommand
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/bin.rs
+++ src/bin.rs
@@ -1,3 +1,15 @@
+use clap::{load_yaml, App};
+
 fn main() {
-    semver::hello();
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from(yaml).get_matches();
+
+    if matches.subcommand_matches("hello").is_some() {
+        semver::hello();
+    } else if let Some(matches) = matches.subcommand_matches("validate") {
+        match matches.value_of("version") {
+            Some(version) => semver::validate::validate(version),
+            None => println!("No version provided"),
+        }
+    }
 }
---
+++ src/validate.rs
@@ -0,0 +1,3 @@
+pub fn validate(version: &str) {
+    println!("Validating version {}", version);
+}
