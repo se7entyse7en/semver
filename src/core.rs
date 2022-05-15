01faa50eef6b368ced138e51d3bfbc16e1e7d5bb
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2022-05-15 14:24:35 +0200

    Make `core` module support prerelease
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/core.rs
+++ src/core.rs
@@ -1,43 +1,157 @@
 #[cfg(test)]
 mod tests;
 use crate::validate;
-use clap::ArgEnum;
+use anyhow;
+use clap;
 use regex::{self, Regex};
 use serde::Deserialize;
-use std::{fmt, str};
+use serde::Serialize;
+use std::{fmt, num, str};
+mod version_manager;
+pub use version_manager::{ExtensionBumpFunc, VersionManager};
 
-const SUPPORTED_PATTERN: &str =
-    r"^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)$";
+const SUPPORTED_PATTERN: &str = r"^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?$";
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug)]
 pub enum VersionError {
     InvalidVersion(String),
+    UnexpectedError(String),
     UnsupportedVersion(String),
+    ParsingError(num::ParseIntError),
+    BumpError(BumpError),
 }
 
-#[derive(Debug, Clone, Eq, PartialEq, ArgEnum, Deserialize)]
-#[serde(rename_all = "lowercase")]
+impl From<num::ParseIntError> for VersionError {
+    fn from(err: num::ParseIntError) -> VersionError {
+        VersionError::ParsingError(err)
+    }
+}
+
+impl From<BumpError> for VersionError {
+    fn from(err: BumpError) -> VersionError {
+        VersionError::BumpError(err)
+    }
+}
+
+#[derive(Debug)]
+pub enum BumpError {
+    AnyError(anyhow::Error),
+    MissingBumpScript,
+    InvalidOperation(String),
+}
+
+impl PartialEq for BumpError {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (BumpError::AnyError(_), _) => false,
+            (_, BumpError::AnyError(_)) => false,
+            (BumpError::MissingBumpScript, BumpError::MissingBumpScript) => true,
+            (BumpError::InvalidOperation(m1), BumpError::InvalidOperation(m2)) => m1 == m2,
+            _ => false,
+        }
+    }
+}
+
+impl From<anyhow::Error> for BumpError {
+    fn from(err: anyhow::Error) -> BumpError {
+        BumpError::AnyError(err)
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Deserialize)]
 pub enum Part {
+    Core(CorePart),
+    Extension(ExtensionPart),
+}
+
+#[derive(Debug)]
+pub struct InvalidPartError {
+    part: String,
+}
+
+impl fmt::Display for InvalidPartError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Invalid part `{}`", self.part)
+    }
+}
+
+impl std::error::Error for InvalidPartError {}
+
+#[derive(Debug, Clone, Eq, PartialEq, Deserialize, clap::ArgEnum)]
+#[serde(rename_all = "lowercase")]
+pub enum CorePart {
     Major,
     Minor,
     Patch,
 }
 
-impl fmt::Display for Part {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::Major => write!(f, "major"),
-            Self::Minor => write!(f, "minor"),
-            Self::Patch => write!(f, "patch"),
+impl str::FromStr for CorePart {
+    type Err = InvalidPartError;
+
+    fn from_str(part: &str) -> Result<Self, Self::Err> {
+        match part {
+            "major" => Ok(CorePart::Major),
+            "minor" => Ok(CorePart::Minor),
+            "patch" => Ok(CorePart::Patch),
+            _ => Err(InvalidPartError {
+                part: part.to_string(),
+            }),
         }
     }
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Clone, Eq, PartialEq, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum ExtensionPart {
+    Prerelease,
+}
+
+impl str::FromStr for ExtensionPart {
+    type Err = InvalidPartError;
+
+    fn from_str(part: &str) -> Result<Self, Self::Err> {
+        match part {
+            "prerelease" => Ok(ExtensionPart::Prerelease),
+            _ => Err(InvalidPartError {
+                part: part.to_string(),
+            }),
+        }
+    }
+}
+
+impl fmt::Display for Part {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Core(CorePart::Major) => write!(f, "major"),
+            Self::Core(CorePart::Minor) => write!(f, "minor"),
+            Self::Core(CorePart::Patch) => write!(f, "patch"),
+            Self::Extension(ExtensionPart::Prerelease) => write!(f, "prerelease"),
+        }
+    }
+}
+
+impl str::FromStr for Part {
+    type Err = InvalidPartError;
+
+    fn from_str(part: &str) -> Result<Self, Self::Err> {
+        match part {
+            "major" => Ok(Part::Core(CorePart::Major)),
+            "minor" => Ok(Part::Core(CorePart::Minor)),
+            "patch" => Ok(Part::Core(CorePart::Patch)),
+            "prerelease" => Ok(Part::Extension(ExtensionPart::Prerelease)),
+            _ => Err(InvalidPartError {
+                part: part.to_string(),
+            }),
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Serialize)]
 pub struct Version {
     pub major: usize,
     pub minor: usize,
     pub patch: usize,
+    pub prerelease: Option<String>,
 }
 
 impl Default for Version {
@@ -48,43 +162,42 @@ impl Default for Version {
 
 impl Version {
     pub fn new() -> Self {
-        Self::with_values(0, 0, 0)
+        Self::with_values(0, 0, 0, None)
     }
 
-    pub fn with_values(major: usize, minor: usize, patch: usize) -> Self {
+    pub fn with_values(
+        major: usize,
+        minor: usize,
+        patch: usize,
+        prerelease: Option<String>,
+    ) -> Self {
         Version {
             major,
             minor,
             patch,
+            prerelease,
         }
     }
 
-    fn extract_part(caps: &regex::Captures, part: &Part) -> usize {
-        caps.name(&part.to_string())
-            .unwrap()
-            .as_str()
-            .parse::<usize>()
-            .unwrap()
-    }
-
-    pub fn bump(&self, part: &Part) -> Self {
+    pub fn get_core_part(&self, part: &CorePart) -> usize {
         match part {
-            Part::Major => self.bump_major(),
-            Part::Minor => self.bump_minor(),
-            Part::Patch => self.bump_patch(),
+            CorePart::Major => self.major,
+            CorePart::Minor => self.minor,
+            CorePart::Patch => self.patch,
         }
     }
 
-    pub fn bump_major(&self) -> Self {
-        Self::with_values(self.major + 1, self.minor, self.patch)
+    pub fn get_extension_part(&self, part: &ExtensionPart) -> Option<String> {
+        match part {
+            ExtensionPart::Prerelease => self.prerelease.to_owned(),
+        }
     }
 
-    pub fn bump_minor(&self) -> Self {
-        Self::with_values(self.major, self.minor + 1, self.patch)
-    }
-
-    pub fn bump_patch(&self) -> Self {
-        Self::with_values(self.major, self.minor, self.patch + 1)
+    fn extract_part<T: str::FromStr>(caps: &regex::Captures, part: &Part) -> Option<T> {
+        match caps.name(&part.to_string()) {
+            Some(found) => found.as_str().parse::<T>().ok(),
+            None => None,
+        }
     }
 }
 
@@ -93,19 +206,45 @@ impl str::FromStr for Version {
 
     fn from_str(raw_version: &str) -> Result<Self, Self::Err> {
         if !validate::validate(raw_version) {
-            Err(VersionError::InvalidVersion(raw_version.to_owned()))
+            Err(VersionError::InvalidVersion(format!(
+                "Invalid version: {}",
+                raw_version.to_owned()
+            )))
         } else {
             let re = Regex::new(SUPPORTED_PATTERN).unwrap();
             if !re.is_match(raw_version) {
-                Err(VersionError::UnsupportedVersion(raw_version.to_owned()))
+                Err(VersionError::UnsupportedVersion(format!(
+                    "Unsupported version: {}",
+                    raw_version.to_owned()
+                )))
             } else {
                 let caps = re.captures(raw_version).unwrap();
-
-                Ok(Self::with_values(
-                    Self::extract_part(&caps, &Part::Major),
-                    Self::extract_part(&caps, &Part::Minor),
-                    Self::extract_part(&caps, &Part::Patch),
-                ))
+                let major = Self::extract_part::<usize>(&caps, &Part::Core(CorePart::Major))
+                    .ok_or_else(|| {
+                        VersionError::UnexpectedError(format!(
+                            "Version {} is valid, but cannot extract 'major' part",
+                            raw_version
+                        ))
+                    })?;
+                let minor = Self::extract_part::<usize>(&caps, &Part::Core(CorePart::Minor))
+                    .ok_or_else(|| {
+                        VersionError::UnexpectedError(format!(
+                            "Version {} is valid, but cannot extract 'minor' part",
+                            raw_version
+                        ))
+                    })?;
+                let patch = Self::extract_part::<usize>(&caps, &Part::Core(CorePart::Patch))
+                    .ok_or_else(|| {
+                        VersionError::UnexpectedError(format!(
+                            "Version {} is valid, but cannot extract 'patch' part",
+                            raw_version
+                        ))
+                    })?;
+                let prerelease = Self::extract_part::<String>(
+                    &caps,
+                    &Part::Extension(ExtensionPart::Prerelease),
+                );
+                Ok(Self::with_values(major, minor, patch, prerelease))
             }
         }
     }
@@ -113,6 +252,9 @@ impl str::FromStr for Version {
 
 impl fmt::Display for Version {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}.{}.{}", self.major, self.minor, self.patch)
+        match &self.prerelease {
+            Some(prerel) => write!(f, "{}.{}.{}-{}", self.major, self.minor, self.patch, prerel),
+            None => write!(f, "{}.{}.{}", self.major, self.minor, self.patch),
+        }
     }
 }
---
+++ src/core/version_manager.rs
@@ -0,0 +1,160 @@
+use crate::core::{BumpError, CorePart, ExtensionPart, Part, Version};
+
+pub trait ExtensionBumpFunc: Fn(&Version) -> Result<String, BumpError> {}
+impl<T> ExtensionBumpFunc for T where T: Fn(&Version) -> Result<String, BumpError> {}
+
+trait Bump {
+    type Value;
+
+    fn bump(&self, version: &Version) -> Result<Self::Value, BumpError>;
+}
+
+struct CoreBumper<'a> {
+    kind: &'a CorePart,
+}
+
+impl<'a> CoreBumper<'a> {
+    pub fn new(kind: &'a CorePart) -> Self {
+        Self { kind }
+    }
+}
+
+impl<'a> Bump for CoreBumper<'a> {
+    type Value = usize;
+
+    fn bump(&self, version: &Version) -> Result<Self::Value, BumpError> {
+        Ok(version.get_core_part(self.kind) + 1)
+    }
+}
+
+struct ExtensionBumper<'a> {
+    #[allow(dead_code)]
+    kind: &'a ExtensionPart,
+    bump_func: Box<dyn ExtensionBumpFunc>,
+}
+
+impl<'a> ExtensionBumper<'a> {
+    pub fn new(bump_func: Box<dyn ExtensionBumpFunc>) -> Self {
+        Self {
+            kind: &ExtensionPart::Prerelease,
+            bump_func,
+        }
+    }
+}
+
+impl<'a> Bump for ExtensionBumper<'a> {
+    type Value = String;
+
+    fn bump(&self, version: &Version) -> Result<Self::Value, BumpError> {
+        (self.bump_func)(version)
+    }
+}
+
+pub struct VersionManager<'a> {
+    core_major_bumper: CoreBumper<'a>,
+    core_minor_bumper: CoreBumper<'a>,
+    core_patch_bumper: CoreBumper<'a>,
+    extension_bumper: Option<ExtensionBumper<'a>>,
+}
+
+impl<'a> Default for VersionManager<'a> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<'a> VersionManager<'a> {
+    pub fn new() -> Self {
+        Self::with_extension_bump_func(None)
+    }
+
+    pub fn with_extension_bump_func(
+        extension_bump_func: Option<Box<dyn ExtensionBumpFunc>>,
+    ) -> Self {
+        Self {
+            core_major_bumper: CoreBumper::new(&CorePart::Major),
+            core_minor_bumper: CoreBumper::new(&CorePart::Minor),
+            core_patch_bumper: CoreBumper::new(&CorePart::Patch),
+            extension_bumper: extension_bump_func.map(|func| ExtensionBumper::new(func)),
+        }
+    }
+
+    pub fn bump(&self, version: &Version, part: &Part) -> Result<Version, BumpError> {
+        match (part, &version.prerelease) {
+            (&Part::Core(_), Some(_)) => Err(BumpError::InvalidOperation(format!(
+                "Cannot bump part {} for version {}, it's a prerelease",
+                part, version
+            ))),
+            (&Part::Extension(ExtensionPart::Prerelease), None) => {
+                Err(BumpError::InvalidOperation(format!(
+                    "Cannot bump prerelease for version {}, it's not a prerelease",
+                    version
+                )))
+            }
+            _ => self.single_part_bump(version, part),
+        }
+    }
+
+    pub fn new_prerelease(&self, version: &Version, part: &CorePart) -> Result<Version, BumpError> {
+        match version.prerelease {
+            Some(_) => Err(BumpError::InvalidOperation(format!(
+                "Cannot create a new prerelease for version {}, it's already a prerelease",
+                version
+            ))),
+            None => self.single_part_bump(
+                &self.single_part_bump(version, &Part::Core(part.to_owned()))?,
+                &Part::Extension(ExtensionPart::Prerelease),
+            ),
+        }
+    }
+
+    pub fn finalize_prerelease(&self, version: &Version) -> Result<Version, BumpError> {
+        match version.prerelease {
+            Some(_) => Ok(Version {
+                major: version.major,
+                minor: version.minor,
+                patch: version.patch,
+                prerelease: None,
+            }),
+            None => Err(BumpError::InvalidOperation(format!(
+                "Cannot finalize version {}, nothing to finalize",
+                version
+            ))),
+        }
+    }
+
+    fn single_part_bump(&self, version: &Version, part: &Part) -> Result<Version, BumpError> {
+        match part {
+            Part::Core(CorePart::Major) => Ok(Version {
+                major: self.core_major_bumper.bump(version)?,
+                minor: version.minor,
+                patch: version.patch,
+                prerelease: version.prerelease.to_owned(),
+            }),
+            Part::Core(CorePart::Minor) => Ok(Version {
+                major: version.major,
+                minor: self.core_minor_bumper.bump(version)?,
+                patch: version.patch,
+                prerelease: version.prerelease.to_owned(),
+            }),
+            Part::Core(CorePart::Patch) => Ok(Version {
+                major: version.major,
+                minor: version.minor,
+                patch: self.core_patch_bumper.bump(version)?,
+                prerelease: version.prerelease.to_owned(),
+            }),
+            Part::Extension(ExtensionPart::Prerelease) => match &self.extension_bumper {
+                Some(ext_bumper) => {
+                    let prerelease = Some(ext_bumper.bump(version)?);
+                    Ok(Version {
+                        major: version.major,
+                        minor: version.minor,
+                        patch: version.patch,
+                        prerelease,
+                    })
+                }
+                None => Err(BumpError::MissingBumpScript),
+            },
+        }
+    }
+}
