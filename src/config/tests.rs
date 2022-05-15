8ff6dc5251b2f895d74e045909db2d6eea1cbd73
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2022-05-15 19:15:16 +0200

    Make `config` module support prerelease
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- src/config/tests.rs
+++ src/config/tests.rs
@@ -1,5 +1,5 @@
 use super::Config;
-use crate::core::Part;
+use crate::core::{CorePart, Part};
 use std::str::FromStr;
 
 #[test]
@@ -22,8 +22,9 @@ default_part = "minor"
     .unwrap();
 
     assert_eq!(config.current_version, "1.0.0");
-    assert_eq!(config.default_part, Part::Minor);
+    assert_eq!(config.default_part, Part::Core(CorePart::Minor));
     assert_eq!(config.path, None);
+    assert_eq!(config.bump_prerelease_func, None);
 
     let mut files: Vec<String> = config.files.into_keys().collect();
     files.sort_unstable();
@@ -91,12 +92,62 @@ default_part = "minor"
 
     for config in [config_1, config_2] {
         assert_eq!(config.current_version, "1.0.0");
-        assert_eq!(config.default_part, Part::Minor);
+        assert_eq!(config.default_part, Part::Core(CorePart::Minor));
         assert_eq!(config.path, None);
+        assert_eq!(config.bump_prerelease_func, None);
 
         let actual_files: Vec<String> = config.files.into_keys().collect();
         let expected_files: Vec<String> = vec![];
 
         assert_eq!(actual_files, expected_files);
     }
+}
+
+#[test]
+fn test_config_with_prerelease() {
+    let config = Config::from_str(
+        r#"
+[semver]
+current_version = "1.0.0"
+default_part = "minor"
+
+  [semver.files]
+
+    [semver.files."test-1.txt"]
+
+    [semver.files."test-2.txt"]
+
+    [semver.files."test-3.txt"]
+
+  [semver.prerelease]
+  bump_script = '''
+var PREFIX = "dev.";
+function bump(version) {
+  var counter = !version.prerelease ? 0 : parseInt(version.prerelease.slice(PREFIX.length));
+  return `${PREFIX}${counter + 1}`;
+}
+'''
+"#,
+    )
+    .unwrap();
+
+    assert_eq!(config.current_version, "1.0.0");
+    assert_eq!(config.default_part, Part::Core(CorePart::Minor));
+    assert_eq!(config.path, None);
+    assert_eq!(
+        config.bump_prerelease_func,
+        Some(
+            r#"var PREFIX = "dev.";
+function bump(version) {
+  var counter = !version.prerelease ? 0 : parseInt(version.prerelease.slice(PREFIX.length));
+  return `${PREFIX}${counter + 1}`;
+}
+"#
+            .to_owned()
+        )
+    );
+
+    let mut files: Vec<String> = config.files.into_keys().collect();
+    files.sort_unstable();
+    assert_eq!(files, ["test-1.txt", "test-2.txt", "test-3.txt"]);
 }
